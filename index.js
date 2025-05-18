e7fd353700d19c7f595d38cdaa4b9cf06a596716
Johnathan Hill <jhill115@terpmail.umd.edu>
2025-05-18 17:43:21 -0400

    trial

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
 
 app.get('/', (req, res) => {
-  res.sendFile('../public/home.html', { root: __dirname });
+  res.sendFile('publicTrial/home.html', { root: __dirname });
 });
 
 app.get('/customers', async (req, res) => {
